4d4b6b5d281ebc455b29e3b079c89362a8a461d3
Metehan Belli <metehanbelli8@gmail.com>
2024-12-01 16:46:14 +0300

    NavigationViewProtocol updated.2

--- ContactSafesApp/Core/Protocols/NavigationViewProtocol.swift
+++ ContactSafesApp/Core/Protocols/NavigationViewProtocol.swift
@@ -5,4 +5,19 @@
 //  Created by Metehan Veliashvili on 1.12.2024.
 //
 
-import Foundation
+import UIKit
+
+protocol NavigationView {
+    func present(_ viewController: UIViewController)
+    func dismiss()
+}
+
+extension NavigationView where Self: UIViewController {
+    func present(_ viewController: UIViewController) {
+        present(viewController, animated: true, completion: nil)
+    }
+
+    func dismiss() {
+        dismiss(animated: true)
+    }
+}
